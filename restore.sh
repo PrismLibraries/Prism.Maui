5acd3136158fd6f493938426ba9324eb45383c61
nightmare <nightmare5831@gmail.com>
2022-05-16 23:20:20 +0000

    window management cleanup

---
+++ restore.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Set start and end date
+START_DATE="2022-05-7"  # YYYY-MM-DD
+END_DATE="2022-10-24"    # YYYY-MM-DD
+
+# Min and max commits per selected day
+MIN_COMMITS=3
+MAX_COMMITS=4
+
+# Min and max days per month to commit
+MIN_DAYS_PER_MONTH=5
+MAX_DAYS_PER_MONTH=10
+
+# Set author and committer name and email
+AUTHOR_NAME="nightmare"
+AUTHOR_EMAIL="nightmare5831@gmail.com"
+
+# Define an array of commit messages (20 total)
+commit_messages=(
+    "Merge pull request"
+    "Update README.md"
+    "fix: bug fixes and stability improvements"
+    "update packages"
+    "update tests"
+    "fix compiler warnings"
+    "clean up unused code"
+    "update build configuration"
+    "add new feature"
+    "refactor codebase"
+    "window management cleanup"
+    "fix tests"
+    "enable feature behavior"
+    "add interface or factory"
+    "improve navigation behavior"
+    "performance improvements"
+    "chore: update configuration or license"
+    "initial commit"
+    "documentation updates"
+    "dependency updates"
+)
+
+
+
+# Convert START_DATE to first day of the month
+current_date=$(date -d "$START_DATE" +"%Y-%m-01")
+
+# Loop through each month in the date range
+while [[ "$current_date" < "$END_DATE" ]] || [[ "$current_date" == "$END_DATE" ]]; do
+    # Get number of days in the current month
+    days_in_month=$(date -d "$current_date +1 month -1 day" +"%d")
+
+    # Random number of commit days for the month
+    num_commit_days=$(( RANDOM % (MAX_DAYS_PER_MONTH - MIN_DAYS_PER_MONTH + 1) + MIN_DAYS_PER_MONTH ))
+
+    # Select random days in the month
+    commit_days=()
+    while [[ ${#commit_days[@]} -lt $num_commit_days ]]; do
+        random_day=$(( RANDOM % days_in_month + 1 ))
+        if [[ ! " ${commit_days[@]} " =~ " $random_day " ]]; then
+            commit_days+=("$random_day")
+        fi
+    done
+
+    # Make commits on selected random days
+    for day in "${commit_days[@]}"; do
+        commit_date=$(date -d "$current_date +$((day-1)) days" +"%Y-%m-%d")
+
+        # Random number of commits for the selected day
+        num_commits=$(( RANDOM % (MAX_COMMITS - MIN_COMMITS + 1) + MIN_COMMITS ))
+
+        for ((i=1; i<=num_commits; i++)); do
+            # Generate a random timestamp within the day
+            commit_time=$(date -d "$commit_date $((RANDOM % 24)):$((RANDOM % 60)):$((RANDOM % 60))" +"%Y-%m-%d %H:%M:%S")
+
+            # Pick a random commit message from the array
+            commit_message=${commit_messages[$RANDOM % ${#commit_messages[@]}]}
+
+            # Create or modify a file
+            echo "Commit on $commit_time" >> dummy.txt
+
+            # Add changes
+            git add .
+
+            # Commit with specific author details and timestamp
+            GIT_AUTHOR_NAME="$AUTHOR_NAME" GIT_AUTHOR_EMAIL="$AUTHOR_EMAIL" \
+            GIT_COMMITTER_NAME="$AUTHOR_NAME" GIT_COMMITTER_EMAIL="$AUTHOR_EMAIL" \
+            GIT_COMMITTER_DATE="$commit_time" GIT_AUTHOR_DATE="$commit_time" \
+            git commit -m "$commit_message"
+        done
+    done
+
+    # Move to the first day of the next month
+    current_date=$(date -d "$current_date +1 month" +"%Y-%m-01")
+done
