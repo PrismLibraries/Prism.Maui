fe39799d081672a6f4fd8dece716824d8e89abfd
nightfairy <nightfairy5831@gmail.com>
2023-06-02 09:56:45 +0000

    enable feature behavior

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2023-01-7"  # YYYY-MM-DD
-END_DATE="2023-04-24"    # YYYY-MM-DD
+START_DATE="2023-06-07"  # YYYY-MM-DD
+END_DATE="2023-09-24"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
